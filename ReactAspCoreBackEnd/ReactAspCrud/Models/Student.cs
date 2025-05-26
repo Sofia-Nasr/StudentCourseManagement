ef610e11f945014da2600ad269f5ea963c968914
Sofia-Nasr <sofianasr22@gmail.com>
2025-05-26 10:32:25 +0500

    Final

---
+++ ReactAspCoreBackEnd/ReactAspCrud/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [JsonPropertyName("stname")]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Course { get; set; }
+}
